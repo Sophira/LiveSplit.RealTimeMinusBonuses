b441b8dfcfc98ed146c2b4b3c6d5bd4a8064cb70
Sophie Hamilton <git-commits@theblob.org>
2017-08-02 23:15:07 +0100

    Fix resizing of time window
    Minor fix to make resizing the in-game time entry window do the same as
    it did for Manual Game Time - resizing the text box and centering it.
    
    Note that this does not add any code to save the size of the window
    between runs. (The position of the window is still saved, as always.)

--- UI/Components/ShitSplitter.Designer.cs
+++ UI/Components/ShitSplitter.Designer.cs
@@ -37,6 +37,9 @@
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
